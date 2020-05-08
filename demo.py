23e0dca6ee0b1429e0498522a8fead04c050a76a
jinsm01 <61052379+jinsm01@users.noreply.github.com>
2020-05-08 14:08:14 +0800

    提交测试

---
+++ demo.py
@@ -0,0 +1,5 @@
+from testing.demo2 import *  # 这个方式__all__变量生效
+
+print(hello)
+
+f()
---
+++ testing/demo2.py
@@ -0,0 +1,11 @@
+# __all__ = ['hello'] ，当前那些模块可被外部调用
+
+hello = 'hello 2'
+
+
+def f():
+    print("he7he")
+
+
+class Demo2:
+    pass
