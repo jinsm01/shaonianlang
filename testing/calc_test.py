249d5f886dc0b5ef6876d590c89f9c1746471d1e
jinsm01 <61052379+jinsm01@users.noreply.github.com>
2020-05-09 20:41:38 +0800

    修改了测试数据获取

--- testing/calc_test.py
+++ testing/calc_test.py
@@ -1,7 +1,12 @@
 import allure
 import pytest
-
 from calc_need.calc import Calc
+from test_data.data_logic import Yaml_data
+
+yaml_data_add = Yaml_data('../test_data/calc_test_add.yaml')
+yaml_data_sub = Yaml_data('../test_data/calc_test_sub.yaml')
+yaml_data_mul = Yaml_data('../test_data/calc_test_mul.yaml')
+yaml_data_div = Yaml_data('../test_data/calc_test_div.yaml')
 
 
 @allure.feature("计算器")
@@ -12,25 +17,25 @@ class TestCalc:
         self.calc = Calc()
 
     @allure.story('加法')
-    @pytest.mark.parametrize('a, b, c', [(0, 0, 0), (0, 11, 11), (9, 9, 18), (0.1, 0.34, 0.44)])
+    @pytest.mark.parametrize('a, b, c', yaml_data_add.data)
     def test_add(self, test_sl_calc, a, b, c):
         result = self.calc.add(a, b)
         assert result == c
 
     @allure.story('除法')
-    @pytest.mark.parametrize('a, b, c', [(0, 1, 0), (4, 4, 1), (9, 4, 2.25), (0, 0, 0), (0.9, 0.3, 3)])
+    @pytest.mark.parametrize('a, b, c', yaml_data_div.data)
     def test_div(self, test_sl_calc, a, b, c):
         result = self.calc.div(a, b)
         assert result == c
 
     @allure.story('减法')
-    @pytest.mark.parametrize('a, b, c', [(0, 11, -11), (9, 9, 0), (31, 0, 31), (0.4, 0.1, 0.3)])
+    @pytest.mark.parametrize('a, b, c', yaml_data_sub.data)
     def test_sub(self, test_sl_calc, a, b, c):
         result = self.calc.sub(a, b)
         assert result == c
 
     @allure.story('乘法')
-    @pytest.mark.parametrize('a, b, c', [(7, 1, 7), (0, 11, 0), (9, 9, 81), (0.1, 0.5, 0.05)])
+    @pytest.mark.parametrize('a, b, c', yaml_data_mul.data)
     def test_mul(self, test_sl_calc, a, b, c):
         result = self.calc.mul(a, b)
         assert result == c
